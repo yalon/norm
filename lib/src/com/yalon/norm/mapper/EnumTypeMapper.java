f59cfb43643944ca23b735ffa68c6573b7d5ec58
Tal Yalon <tal.yalon@gmail.com>
2011-02-19 00:01:44 +0200

    - removed unnecessary import, no deprecations now!

--- lib/src/com/yalon/norm/mapper/EnumTypeMapper.java
+++ lib/src/com/yalon/norm/mapper/EnumTypeMapper.java
@@ -3,7 +3,6 @@ package com.yalon.norm.mapper;
 import java.lang.reflect.Array;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
-import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
